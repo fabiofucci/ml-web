401f96be6e90eaf8d71494d81b755f08f8fe1b32
f.fucci <in1pgqbij>
2018-10-31 16:30:33 +0100

    aggiunta la directory .s2i

--- .s2i/bin/run.sh
+++ .s2i/bin/run.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-echo "Avvio dell'applicazione"
-exec ng serve --prod=true --host=$NG_APP_HOST --port=$NG_APP_PORT
+##!/bin/bash
+## echo "Avvio dell'applicazione"
+## exec ng serve --prod=true --host=$NG_APP_HOST --port=$NG_APP_PORT
