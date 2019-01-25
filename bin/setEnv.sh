15def23cc8acd32d0501e1daf474b0d622c5e4c6
f.fucci <in1pgqbij>
2019-01-25 17:15:23 +0100

    Modifiche al sed

--- bin/setEnv.sh
+++ bin/setEnv.sh
@@ -7,10 +7,10 @@ echo "\$ML_ARTIST_DISCOVERY_ARTIST_URL:" $ML_ARTIST_DISCOVERY_ARTIST_URL
 echo "\$ML_ARTIST_FBPAGE_SEARCH_URL:" $ML_ARTIST_FBPAGE_SEARCH_URL
 echo "\$ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL:" $ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL
 
-sed -i s/@#ML_ARTIST_DISCOVERY_SEARCH_URL#@/$ML_ARTIST_DISCOVERY_SEARCH_URL/ /url/local/app/ml-web/src/environments/environment.prod.ts
-sed -i s/@#ML_ARTIST_DISCOVERY_ARTIST_URL#@/$ML_ARTIST_DISCOVERY_ARTIST_URL/ /url/local/app/ml-web/src/environments/environment.prod.ts
-sed -i s/@#ML_ARTIST_FBPAGE_SEARCH_URL#@/$ML_ARTIST_FBPAGE_SEARCH_URL/ /url/local/app/ml-web/src/environments/environment.prod.ts
-sed -i s/@#ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL#@/$ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL/ /url/local/app/ml-web/src/environments/environment.prod.ts
+sed -i s^@#ML_ARTIST_DISCOVERY_SEARCH_URL#@^$ML_ARTIST_DISCOVERY_SEARCH_URL^ /url/local/app/ml-web/src/environments/environment.prod.ts
+sed -i s^@#ML_ARTIST_DISCOVERY_ARTIST_URL#@^$ML_ARTIST_DISCOVERY_ARTIST_URL^ /url/local/app/ml-web/src/environments/environment.prod.ts
+sed -i s^@#ML_ARTIST_FBPAGE_SEARCH_URL#@^$ML_ARTIST_FBPAGE_SEARCH_URL^ /url/local/app/ml-web/src/environments/environment.prod.ts
+sed -i s^@#ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL#@^$ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL^ /url/local/app/ml-web/src/environments/environment.prod.ts
 
 echo "FILE DELLE VARIABILI DI AMBIENTE:"
 cat /url/local/app/ml-web/src/environments/environment.prod.ts
