76abdad0f187abd3ffb7ea2d99d06ac19b456a28
f.fucci <in1pgqbij>
2019-01-30 10:13:53 +0100

    Sostituzione globale

--- bin/run.sh
+++ bin/run.sh
@@ -8,10 +8,10 @@ echo "\$ML_ARTIST_FBPAGE_SEARCH_URL:" $ML_ARTIST_FBPAGE_SEARCH_URL
 echo "\$ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL:" $ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL
 
 echo "PREPARAZIONE DEL FILE .js DELLE VARIABILI DI AMBIENTE:"
-sed -i s^@#ML_ARTIST_DISCOVERY_SEARCH_URL#@^$ML_ARTIST_DISCOVERY_SEARCH_URL^ /usr/local/apache2/htdocs/assets/js/env.js
-sed -i s^@#ML_ARTIST_DISCOVERY_ARTIST_URL#@^$ML_ARTIST_DISCOVERY_ARTIST_URL^ /usr/local/apache2/htdocs/assets/js/env.js
-sed -i s^@#ML_ARTIST_FBPAGE_SEARCH_URL#@^$ML_ARTIST_FBPAGE_SEARCH_URL^ /usr/local/apache2/htdocs/assets/js/env.js
-sed -i s^@#ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL#@^$ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL^ /usr/local/apache2/htdocs/assets/js/env.js
+sed -i s^@#ML_ARTIST_DISCOVERY_SEARCH_URL#@^$ML_ARTIST_DISCOVERY_SEARCH_URL^g /usr/local/apache2/htdocs/assets/js/env.js
+sed -i s^@#ML_ARTIST_DISCOVERY_ARTIST_URL#@^$ML_ARTIST_DISCOVERY_ARTIST_URL^g /usr/local/apache2/htdocs/assets/js/env.js
+sed -i s^@#ML_ARTIST_FBPAGE_SEARCH_URL#@^$ML_ARTIST_FBPAGE_SEARCH_URL^g /usr/local/apache2/htdocs/assets/js/env.js
+sed -i s^@#ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL#@^$ML_ARTIST_FBPAGE_GOOGLE_SEARCH_URL^g /usr/local/apache2/htdocs/assets/js/env.js
 
 echo "FILE DELLE VARIABILI DI AMBIENTE:"
 cat /usr/local/apache2/htdocs/assets/js/env.js
